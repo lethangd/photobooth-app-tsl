e7f6be2e9a642b404b9291210a16072d4f5b3595
Michael G <michael@groene.de>
2026-07-02 14:43:57 +0200

    add benchmark to test encode animations using pyav instead pillow

--- src/tests/benchmarks/encode_animation_test.py
+++ src/tests/benchmarks/encode_animation_test.py
@@ -135,12 +135,150 @@ def pyav_encode_mp4(images: list[Image.Image], frame_duration: int = 125):
         return tmp.read()  # bytes of the encoded file
 
 
+def pyav_encode_gif(images: list[Image.Image], frame_duration: int = 125):
+    # ref https://github.com/PyAV-Org/PyAV/blob/main/examples/numpy/generate_video_with_pts.py
+    fps = round(1.0 / (frame_duration / 1000.0))  # frame_duration is in [ms], normize to [s] for pyav
+
+    n = len(images)
+    cycle = [*range(n), *range(n - 2, 0, -1)]
+    repeat = 1
+    sequence = cycle * repeat
+
+    with tempfile.NamedTemporaryFile(suffix=".gif", delete=False) as tmp:
+        container = av.open(tmp.name, mode="w")
+        stream = container.add_stream("gif", rate=fps)
+        # stream.codec_context.time_base = Fraction(1, fps)
+        stream.width = images[0].width
+        stream.height = images[0].height
+        stream.pix_fmt = "rgb8"
+
+        my_pts = 0  # [seconds]
+        for idx in sequence:
+            frame = av.VideoFrame.from_image(images[idx])
+
+            frame = frame.reformat(format="rgb8")
+            frame.pts = my_pts
+            my_pts += 1.0
+
+            for packet in stream.encode(frame):
+                container.mux(packet)
+
+        # last frame duplication seems not needed here, it plays correctly without
+
+        # Flush stream
+        for packet in stream.encode():
+            container.mux(packet)
+
+        # Close the file
+        container.close()
+        # shutil.copy(tmp.name, "test.gif")
+        return tmp.read()  # bytes of the encoded file
+
+
+def pyav_encode_avif(images: list[Image.Image], frame_duration: int = 125):
+    # ref https://github.com/PyAV-Org/PyAV/blob/main/examples/numpy/generate_video_with_pts.py
+    fps = round(1.0 / (frame_duration / 1000.0))  # frame_duration is in [ms], normize to [s] for pyav
+    print(av.codecs_available)
+    n = len(images)
+    cycle = [*range(n), *range(n - 2, 0, -1)]
+    repeat = 1
+    sequence = cycle * repeat
+
+    with tempfile.NamedTemporaryFile(suffix=".avif", delete=False) as tmp:
+        container = av.open(tmp.name, mode="w")
+        stream = container.add_stream("libsvtav1", rate=fps)
+        assert isinstance(stream, av.VideoStream)
+        # stream.codec_context.time_base = Fraction(1, fps)
+        stream.width = images[0].width
+        stream.height = images[0].height
+        stream.pix_fmt = "yuv420p10le"
+        # 3. SVT-AV1 spezifische Parameter setzen
+        stream.options = {
+            "crf": "26",  # Qualität (0-63, niedriger = besser)
+            "preset": "10",  # größer= höhere Geschwindigkeit (0-13, 6-8 ist optimal für Alltag)
+            "svtav1-params": "tune=0",  # 0 = Visuelle Qualität (VQ), 1 = PSNR
+        }
+
+        my_pts = 0  # [seconds]
+        for idx in sequence:
+            frame = av.VideoFrame.from_image(images[idx])
+
+            frame = frame.reformat(format="yuv420p10le")
+            frame.pts = my_pts
+            my_pts += 1.0
+
+            for packet in stream.encode(frame):
+                container.mux(packet)
+
+        # last frame duplication seems not needed here, it plays correctly without
+
+        # Flush stream
+        for packet in stream.encode():
+            container.mux(packet)
+
+        # Close the file
+        container.close()
+        # shutil.copy(tmp.name, "test.avif")
+        return tmp.read()  # bytes of the encoded file
+
+
+def pyav_encode_webp(images: list[Image.Image], frame_duration: int = 125):
+    # ref https://github.com/PyAV-Org/PyAV/blob/main/examples/numpy/generate_video_with_pts.py
+    fps = round(1.0 / (frame_duration / 1000.0))  # frame_duration is in [ms], normize to [s] for pyav
+    print(av.codecs_available)
+    n = len(images)
+    cycle = [*range(n), *range(n - 2, 0, -1)]
+    repeat = 1
+    sequence = cycle * repeat
+
+    with tempfile.NamedTemporaryFile(suffix=".webp", delete=False) as tmp:
+        container = av.open(tmp.name, mode="w")
+        stream = container.add_stream("libwebp", rate=fps)
+        assert isinstance(stream, av.VideoStream)
+        # stream.codec_context.time_base = Fraction(1, fps)
+        stream.width = images[0].width
+        stream.height = images[0].height
+        stream.pix_fmt = "yuv420p"
+        # 3. WebP-spezifische Optionen setzen
+        stream.options = {
+            "lossless": "0",  # 0 = Verlustbehaftet (kleine Datei), 1 = Verlustfrei (groß)
+            "quality": "80",  # Qualität von 0-100 (nur aktiv bei lossless=0)
+            "compression_level": "2",  # Encoding-Aufwand von 0-6 (höher = kleinere Datei, langsamer)
+            "loop": "0",  # 0 bedeutet unendlicher Loop (Standard)
+        }
+
+        my_pts = 0  # [seconds]
+        for idx in sequence:
+            frame = av.VideoFrame.from_image(images[idx])
+
+            frame = frame.reformat(format="yuv420p")
+            frame.pts = my_pts
+            my_pts += 1.0
+
+            for packet in stream.encode(frame):
+                container.mux(packet)
+
+        # last frame duplication seems not needed here, it plays correctly without
+
+        # Flush stream
+        for packet in stream.encode():
+            container.mux(packet)
+
+        # Close the file
+        container.close()
+        # shutil.copy(tmp.name, "test.webp")
+        return tmp.read()  # bytes of the encoded file
+
+
 @pytest.fixture(
     params=[
         "pillow_encode_gif",
         "pillow_encode_webp",
         "pillow_encode_avif",
         "pyav_encode_mp4",
+        "pyav_encode_gif",
+        "pyav_encode_avif",
+        "pyav_encode_webp",
     ]
 )
 def library(request):
