7f2c00295cb160c48087681abad5a511868a6d75
Michael G <michael@groene.de>
2026-08-15 09:49:00 +0200

    remove outdated ffmpeg boomerang step

--- src/tests/benchmarks/encode_boomerang_mp4_test.py
+++ src/tests/benchmarks/encode_boomerang_mp4_test.py
@@ -5,19 +5,9 @@ import pytest
 
 from photobooth.services.mediaprocessing.context import VideoContext
 from photobooth.services.mediaprocessing.pipeline import Pipeline
-from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepFormerFfmpeg, LoopStep
+from photobooth.services.mediaprocessing.steps.video import BoomerangStep, LoopStep
 
 logger = logging.getLogger(name=None)
-
-
-def video_boomerangFfmpeg_stage():
-    video_in = Path("src/tests/assets/video.mp4")
-
-    context = VideoContext(video_in)
-    steps = [BoomerangStepFormerFfmpeg(1)]
-    pipeline = Pipeline[VideoContext](*steps)
-    pipeline(context)
-    assert context.video_processed
 
 
 def video_boomerangPyav_stage():
@@ -39,11 +29,6 @@ def video_loopPyav_stage():
     pipeline = Pipeline[VideoContext](*steps)
     pipeline(context)
     assert context.video_processed
-
-
-@pytest.mark.benchmark(group="create_boomerang")
-def test_encode_boomerang_ffmpeg(benchmark):
-    benchmark(video_boomerangFfmpeg_stage)
 
 
 @pytest.mark.benchmark(group="create_boomerang")
--- src/tests/tests/mediaprocessing/test_videosteps.py
+++ src/tests/tests/mediaprocessing/test_videosteps.py
@@ -10,7 +10,7 @@ import pytest
 
 from photobooth.services.mediaprocessing.context import VideoContext
 from photobooth.services.mediaprocessing.pipeline import Pipeline
-from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepFormerFfmpeg, LoopStep
+from photobooth.services.mediaprocessing.steps.video import BoomerangStep, LoopStep
 
 from ..util import video_duration
 
@@ -64,24 +64,6 @@ def test_video_boomerang_stage():
         logger.warning(container_in.streams.video[0].codec_context.framerate)
         logger.warning(container_in.streams.video[0].codec_context.rate)
         logger.warning(container_in.streams.video[0].codec_context.pix_fmt)
-
-
-def test_video_boomerangPyav_stage():
-    video_in = Path("src/tests/assets/video.mp4")
-    boomerang_speed = 3
-
-    context = VideoContext(video_in)
-    steps = [BoomerangStepFormerFfmpeg(boomerang_speed)]
-    pipeline = Pipeline[VideoContext](*steps)
-    pipeline(context)
-    assert context.video_processed
-    video_out = context.video_processed
-
-    # boomerang reverses video so double length
-    in_dur = video_duration(video_in)
-    out_dur = video_duration(video_out)
-
-    assert out_dur == pytest.approx(in_dur * 2.0 * (1.0 / boomerang_speed), abs=0.5)
 
 
 def test_video_loop_stage():
